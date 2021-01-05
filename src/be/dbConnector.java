ef28c8d70b9d906d2238cd35475d84ca822f8c5e
jbn108 <jbuus9@gmail.com>
2021-01-05 16:28:37 +0100

    Created dbConnection file

--- src/be/dbConnector.java
+++ src/be/dbConnector.java
@@ -1,4 +1,27 @@
 package be;
 
+import java.sql.Connection;
+
 public class dbConnector {
+
+
+    private SQLServerDataSource dataSource;
+
+    public MyDatabaseConnector(){
+        dataSource = new SQLServerDataSource();
+        dataSource.setServerName("10.176.111.31");
+        dataSource.setDatabaseName("MyTunes2000");
+        dataSource.setUser("CSe20A_23");
+        dataSource.setPassword("DiscoDucks");
+        dataSource.setPortNumber(1433);
+    }
+
+    /**
+     * Establish a connection to the database.
+     * @return the connection to the data source.
+     * @throws SQLServerException
+     */
+    public Connection getConnection() throws SQLServerException {
+        return dataSource.getConnection();
+    }
 }

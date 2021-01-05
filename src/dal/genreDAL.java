93b0d46c4791273ca2a8f1ec58e60f039aeff3d5
jbn108 <jbuus9@gmail.com>
2021-01-05 15:30:57 +0100

    Setup of dal, bll

---
+++ src/dal/genreDAL.java
@@ -0,0 +1,29 @@
+package dal;
+
+import be.Playlist;
+import be.dbConnector;
+
+import java.util.List;
+
+public class genreDAL {
+    private dbConnector dbCon = new dbConnector();
+
+    // TODO
+    public void addGenre(){}
+
+    // TODO
+    public void deleteGenre(){}
+
+    // TODO
+    public List<String> getAllGenres(){
+        return null;
+    }
+
+    // TODO
+    public List<Playlist> getPlaylist(){
+        return null;
+    }
+
+    // TODO
+    // TODO
+}

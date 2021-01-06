5a7c160e3027a0a256931b96f07afd8d35771bb5
jbn108 <jbuus9@gmail.com>
2021-01-06 01:13:37 +0100

    Dbconnection set up + addGenre method + dbTest class created

--- src/dal/genreDAL.java
+++ src/dal/genreDAL.java
@@ -2,14 +2,32 @@ package dal;
 
 import be.Playlist;
 import be.dbConnector;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 public class genreDAL {
-    private dbConnector dbCon = new dbConnector();
+    private dbConnector dbCon;
+
+    public genreDAL() throws IOException {
+        dbCon = new dbConnector();
+    }
+
+
 
     // TODO
-    public void addGenre(){}
+    public void addGenre(String genre) throws SQLException {
+        try (Connection con = dbCon.getConnection()) {
+
+            PreparedStatement pSql = con.prepareStatement("INSERT INTO Genre VALUES(?)");
+            pSql.setString(1, genre);
+            pSql.execute();
+        }
+    }
 
     // TODO
     public void deleteGenre(){}
--- src/dal/movieDAL.java
+++ src/dal/movieDAL.java
@@ -3,10 +3,14 @@ package dal;
 import be.Movie;
 import be.dbConnector;
 
+import java.io.IOException;
 import java.util.List;
 
 public class movieDAL {
     private dbConnector dbCon = new dbConnector();
+
+    public movieDAL() throws IOException {
+    }
 
 
     // TODO
